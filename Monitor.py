0db7a913560c8abd5a07b27586015865d43504f9
Emmett Cowan <knockanillaun@gmail.com>
2021-01-09 15:38:35 +0000

    formated code & comments

--- Monitor.py
+++ Monitor.py
@@ -7,62 +7,63 @@
 #########################################################
 
 
-import time
-import win32gui     
-import uiautomation as auto
-import json
-import datetime as dt
-import pymongo
-from threading import Thread
-import tkinter as tk
+import time                     # timestamps
+import win32gui                 # active window info
+import uiautomation as auto     # url infor
+import json                     # loggin
+import datetime as dt           # timestaps
+import pymongo                  # mongodb
+from threading import Thread    # gui threading
+import tkinter as tk            # gui
 
 
 user = ""
 thread = None
 
 class detectionThread(Thread):
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")    # mongodb connection
     active_window = ""
     window = ""
     startTime = time.time()
     firstRun = True
     threadRunning = False
 
-    def activeWindow(self):
+    def activeWindow(self):             # get the name of the forground window
         self.activeWindowName = None
         self.activeWindowName = win32gui.GetWindowText(win32gui.GetForegroundWindow())
         return self.activeWindowName
 
 
-    def chromeUrl(self):
+    def chromeUrl(self):                            # get url from chrome
         window = win32gui.GetForegroundWindow()
         chromeControl = auto.ControlFromHandle(window)
         chromeWindow = chromeControl.EditControl()
         return '' + chromeWindow.GetValuePattern().Value
 
-    def urlStrip(self, url):
+    def urlStrip(self, url):                       # strip url down 
         string_list = url.split('/')
         return string_list[0]
 
-    def dbPost(self, activityData):
+    def dbPost(self, activityData):             # post  data to mongo
         mydb = self.myclient["Monitor"]
         mycol = mydb[user]
-        print(mycol)
+        print(mycol)                        #swap these comments to post to db or for testing
         #mycol.insert_one(activityData)
 
 
-    def run(self):
+    def run(self):                  # thread for main applicaion code
         self.threadRunning = True
 
         while self.threadRunning:
             self.active_window = self.activeWindow()      #get current windows app
+
             # if 'Google Chrome' in self.active_window:     #get url if in chrome app 
-            #     url = self.chromeUrl()
+            #     url = self.chromeUrl()                       #problem with threading
             #     self.active_window = self.urlStrip(url)
 
-            if self.active_window != '':                
-                if self.window != self.active_window:
-                    if self.active_window != '':
+            if self.active_window != '':                    #  checking & removing
+                if self.window != self.active_window:       #  empty 
+                    if self.active_window != '':            #  return values
                         
                         if not self.firstRun:
                             endTime = time.time()
@@ -79,10 +80,11 @@ class detectionThread(Thread):
 
     def stopThread(self):
         self.threadRunning = False
-                
 
-   
-def launch_thread():
+                
+#---------------------------------------- GUI CODE --------------------------------------#
+ 
+def launch_thread():        # gui start thread
     global thread
     if thread:
         print("Monitor already Started")
@@ -92,7 +94,7 @@ def launch_thread():
         thread.start()
 
 
-def stopThread_thread():
+def stopThread_thread():       # gui stop thread
     global thread
     if thread:
         thread.stopThread()
@@ -101,19 +103,19 @@ def stopThread_thread():
         print("Nothing to stop")
 
 
-def login():
+def login():            # gui login 
     global user
     x1 = entry1.get()
     user = x1
     print(x1)
     
 
-window= tk.Tk()
+window= tk.Tk()         #create tkinr object for gui
 
-canvas = tk.Canvas(window, width = 400, height = 300,  relief = 'raised')
-canvas.pack()
+canvas = tk.Canvas(window, width = 400, height = 300,  relief = 'raised')       # create canvas 
+canvas.pack()                                                                   # set layout and fill widgets
 
-label1 = tk.Label(window, text='Monitor V0.5')
+label1 = tk.Label(window, text='Monitor V0.6')
 label1.config(font=('helvetica', 14))
 canvas.create_window(200, 25, window=label1)
 
@@ -137,4 +139,4 @@ canvas.create_window(250,250, window=button3)
 
 window.mainloop()
 
-                
+#----------------------------------------------------------------------------------------#               
